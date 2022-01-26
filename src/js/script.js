32ea981d94fb628070c44fda9217dafac6d95368
Vladimir <homie43@yandex.ru>
2022-01-26 18:30:55 +0300

    menu done

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger'), /* обращаемся к гамбургеру */
+        menu = document.querySelector('.menu'), /* обращаеимся к меню */
+        closeElem = document.querySelector('.menu__close'); /* обращаемся к крестику */
+
+hamburger.addEventListener('click',() => { /* при клике на hamburge, будет добавляться(add) к menu класс активности active */
+        menu.classList.add('active'); /* обращаясь к списку классов класса меню, мы будем добавлять класс active */
+});
+
+closeElem.addEventListener('click',() => { /* при клике на крестик(closeElem) будет убираться(remove) класс активности active у menu */
+    menu.classList.remove('active');
+});
